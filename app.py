f379bcaa16bd59404dda2d70baf277e34076c208
antarrikshh <mukatiantariksh22@gmail.com>
2026-09-07 14:52:55 +0530

    Add subtract feature

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ def multiply(a, b):
     return a * b
 
 
+def subtract(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
     print("Application is running")
     print("2 + 3 =", add(2, 3))
+    print("5 - 3 =", subtract(5, 3))
