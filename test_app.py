d67ad1bb52e96fd811d2a7adb352aae914f1e005
antarrikshh <mukatiantariksh22@gmail.com>
2026-09-07 15:02:15 +0530

    Replace add and multiply tests with calculate test

--- test_app.py
+++ test_app.py
@@ -1,9 +1,5 @@
-from app import add, multiply
+from app import calculate
 
 
-def test_add():
-    assert add(2, 3) == 5
-
-
-def test_multiply():
-    assert multiply(2, 3) == 6
+def test_calculate():
+    assert calculate("2 + 3") == 5
